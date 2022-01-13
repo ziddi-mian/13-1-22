88e8388cea4a379f7edfe2e17ae843276c828eda
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:29:58 +0500

    jzs

--- script.js
+++ script.js
@@ -444,11 +444,6 @@ console.log(hours_with_zeroes(dt));
 
 
 
-
-
-
-
-
 // Write a JavaScript function to get 24-hour format of an hour without leading zeros.   
 // Test Data :
 // dt = new Date(1989, 10, 1);
