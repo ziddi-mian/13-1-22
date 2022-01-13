527dcf2bbd88e19b05c15774e5c4d80b1fe90771
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:30:43 +0500

    js

--- script.js
+++ script.js
@@ -465,11 +465,6 @@ console.log(hours_without_zeroes(dt));
 
 
 
-
-
-
-
-
 // Write a JavaScript function to get minutes with leading zeros (00 to 59).   
 // Test Data :
 // dt = new Date(1989, 10, 1);
