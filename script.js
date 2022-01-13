6a1db4bb98b30e371f02bb9491f3144c01f94433
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:41:02 +0500

    js

--- script.js
+++ script.js
@@ -1083,13 +1083,6 @@ console.log(timezone_offset_in_seconds(dt));
 
 
 
-
-
-
-
-
-
-
 // Write a JavaScript function to get timezone offset in seconds.   
 // Note : The offset for timezones west of UTC is always negative, and for those east of UTC is always positive.
 // Test Data :
