de80c6a42b1d332d86e368eb82485813f68e11ad
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:33:16 +0500

    jds

--- script.js
+++ script.js
@@ -654,10 +654,6 @@ console.log(long_Days(dt));
 
 
 
-
-
-
-
 // Write a JavaScript function to get a full textual representation of the day of the week (Sunday through Saturday).   
 // Test Data :
 // dt = new Date(2015, 10, 1);
