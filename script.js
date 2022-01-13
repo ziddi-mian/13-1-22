36eca3a87e07a27700d05d0af26523fe8cb33449
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:31:54 +0500

    u9oi

--- script.js
+++ script.js
@@ -552,14 +552,6 @@ console.log(lower_am_pm(dt));
 
 
 
-
-
-  
-
-
-
-  
-
     //   Write a JavaScript function to swatch Internet time (000 through 999).   
 // Test Data :
 // dt = new Date(1989, 10, 1);
