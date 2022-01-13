8aad0f2986840c214adfdce1fe0bc5086c949f5c
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:34:36 +0500

    js

--- script.js
+++ script.js
@@ -786,8 +786,6 @@ dt = new Date(2015, 10, 1);
 console.log(numeric_month(dt));
 
 
-
-
 // Write a JavaScript function to get a short textual representation of a month, three letters (Jan through Dec).   
 // Test Data :
 // dt = new Date(2015, 10, 1);
