79ca995a928b47e72af3a34245399f4ce084e3e0
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:30:58 +0500

    jds

--- script.js
+++ script.js
@@ -485,11 +485,6 @@ console.log(minutes_with_leading_zeros(dt));
 
 
 
-
-
-
-
-
 // Write a JavaScript function to get seconds with leading zeros (00 through 59).   
 // Test Data :
 // dt = new Date(1989, 10, 1);
