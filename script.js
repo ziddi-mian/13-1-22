31e0fc1ba454744ebcdd30f0dd8e2e3202de587a
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-13 07:35:01 +0500

    js

--- script.js
+++ script.js
@@ -825,15 +825,6 @@ dt = new Date(2015, 10, 1);
 console.log(full_year(dt));
 
 
-
-
-
-
-
-
-
-
-
 // Write a JavaScript function to get a two digit representation of a year.   
 // Examples : 79 or 04
 // Test Data :
